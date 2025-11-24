cbb86a35339f849d106e88ed4c5f2e879310e61a
Luke McMahon <lukemcmahon@canva.com>
2025-11-24 15:29:03 +1100

    .

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,17 @@
 type APIConfig = {
 	fileServerHits: number;
+	dbURL: string;
 };
 
 export const config: APIConfig = {
-  fileServerHits: 0,
+	fileServerHits: 0,
+	dbURL: envOrThrow("DB_URL"),
 };
+
+function envOrThrow(key: string): string {
+	if (!process.env[key]) {
+		throw new Error(`${key} does not exit in .env`);
+	}
+
+	return process.env[key];
+}
---
+++ src/db/queries/users.ts
@@ -0,0 +1,9 @@
+
+import { db } from "../index.js";
+import { NewUser, users } from "../schema.js";
+
+export async function createUser(user: NewUser) {
+	const [result] = await db.insert(users).values(user).onConflictDoNothing().returning();
+	return result;
+}
+
